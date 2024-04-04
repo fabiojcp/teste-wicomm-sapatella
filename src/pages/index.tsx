78a73d611287ab4848502cdea3fa3b2b4e8a72e4
Fábio Casanova <fabio50019@gmail.com>
2024-04-03 21:33:00 -0300

    feat:seo

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Seo from "@/components/Seo";
+
+const HomePage = () => {
+  return (
+    <>
+      <Seo
+        title="Sapatella"
+        description="Oi, a gente já se conhece? Muito prazer, Sapatella! Somos uma Marca de moda que oferece sapatos e acessórios feitos para te encantar. Vamos juntas?"
+      />
+      <p>aaaaaaaaaaaaaaaaaaa</p>
+    </>
+  );
+};
+
+export default HomePage;
